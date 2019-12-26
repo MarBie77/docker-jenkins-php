7f2d19dd44c48478827ed94ccd96708882f0896d
Martin Biermair <martin@biermair.at>
2019-12-26 23:06:35 +0100

    updating to phpunit 8.x

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -x
 # update global dependencies
 composer global config minimum-stability dev
 composer global config prefer-stable true
-composer global require --no-interaction --no-progress --no-suggest phpunit/phpunit:^7.0 squizlabs/php_codesniffer phploc/phploc pdepend/pdepend phpmd/phpmd sebastian/phpcpd mayflower/php-codebrowser theseer/phpdox:dev-master
+composer global require --no-interaction --no-progress --no-suggest phpunit/phpunit:^8.0 squizlabs/php_codesniffer phploc/phploc pdepend/pdepend phpmd/phpmd sebastian/phpcpd mayflower/php-codebrowser theseer/phpdox:dev-master
 
 if [ ! -e $JENKINS_HOME/jobs/php-template/config.xml ]
 then
